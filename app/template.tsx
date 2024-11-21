8f39f1892b42dc4585a081c8a1cea15daed7d7e5
egarrisxn <egarrisxn@gmail.com>
2024-11-21 02:16:29 -0500

    first commit

---
+++ app/template.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+// On load fade in animation setup
+const duration = 0.4
+const variants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    transition: {
+      duration: duration,
+      delay: 0.3,
+      ease: 'linear',
+      when: 'beforeChildren',
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: duration, ease: 'linear', when: 'beforeChildren' },
+  },
+}
+
+// Wrapping with framer motion
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+    >
+      {children}
+    </motion.div>
+  )
+}
